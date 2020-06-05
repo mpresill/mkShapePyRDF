20cfda235dd105a264914c5248c76cd24e24826f
mpresill <26869915+mpresill@users.noreply.github.com>
2020-06-05 11:43:16 +0200

    Update launch_script.sh

--- launch_script.sh
+++ launch_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc8-opt/setup.sh
 # python latinoRDF_numpy_exporter.py --config Full2018v6s5_v2 \
 #     --cut boost_sig --ver v11 -o /eos/user/d/dvalsecc/www/VBSPlots/DNN_archive \
 #     -s VVV Wjets VBS DY VBF-V top VV \
@@ -7,13 +7,18 @@
 
 
 
-python latinoRDF_numpy_exporter.py --config Full2017v6s5_v2 \
-    --cut res_sig --ver v11 -o /eos/user/d/dvalsecc/www/VBSPlots/DNN_archive \
-    -s VVV Wjets VBS DY VBF-V top VV \
-    --debug --functions functions.hh
+#python latinoRDF_numpy_exporter.py --config Full2017v6s5_v2 \
+#    --cut res_sig --ver v11 -o /eos/user/d/dvalsecc/www/VBSPlots/DNN_archive \
+#    -s VVV Wjets VBS DY VBF-V top VV \
+#    --debug --functions functions.hh
 
 
-python latinoRDF_numpy_exporter.py --config Full2017v6s5_v2 \
-    --cut boost_sig --ver v11 -o /eos/user/d/dvalsecc/www/VBSPlots/DNN_archive \
-    -s VVV Wjets VBS DY VBF-V top VV \
-    --debug --functions functions.hh
+#python latinoRDF_numpy_exporter.py --config Full2017v6s5_v2 \
+#    --cut boost_sig --ver v11 -o /eos/user/d/dvalsecc/www/VBSPlots/DNN_archive \
+#    -s VVV Wjets VBS DY VBF-V top VV \
+#    --debug --functions functions.hh
+
+python latinoRDF_numpy_exporter.py --config VBS_ZV_2017v6 \
+    --cut dummy --ver v11 -o VBS_ZV_2017v6 \
+    -s WJets \
+    --debug
