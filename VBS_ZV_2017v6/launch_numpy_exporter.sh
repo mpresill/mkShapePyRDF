6603e9af3ec05ddda793b6a053210506ecb8be53
mpresill <26869915+mpresill@users.noreply.github.com>
2020-06-05 11:41:16 +0200

    Create launch_numpy_exporter.sh

---
+++ VBS_ZV_2017v6/launch_numpy_exporter.sh
@@ -0,0 +1,3 @@
+source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc8-opt/setup.sh
+
+python latinoRDF_numpy_exporter.py  --config-dir ./VBS_ZV_2017v6  --cut dummy  --outputdir ./VBS_ZV_2017v6  --vers v1  --samples WJets  --debug
