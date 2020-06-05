0a04f0660b95fe7210f36764ba739faf89e3d23d
mpresill <26869915+mpresill@users.noreply.github.com>
2020-06-05 11:42:00 +0200

    Delete launch_numpy_exporter.sh

--- VBS_ZV_2017v6/launch_numpy_exporter.sh
+++
@@ -1,3 +0,0 @@
-source /cvmfs/sft.cern.ch/lcg/views/LCG_96bpython3/x86_64-centos7-gcc8-opt/setup.sh
-
-python latinoRDF_numpy_exporter.py  --config-dir .  --cut dummy  --outputdir .  --vers v1  --samples WJets  --debug
